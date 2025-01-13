b74477f6c90a48202b5b305338a13e8b3bb6b191
Denizhan Şahin <denizhansahin@yaani.com>
2025-01-13 14:35:39 +0300

    first commit

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,6 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { isDev } from './utils.js';
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    console.log('isDev:', isDev());
+    console.log('Environment:', process.env.NODE_ENV);
+    if (isDev()) {
+        console.log('Development mode detected, loading localhost...');
+        mainWindow.loadURL('http://localhost:5123');
+    }
+    else {
+        console.log('Production mode detected, loading built files...');
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    }
 });
